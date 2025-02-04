5ad9c5dbcd09a3108fd88e219569640e8773a000
Han Ju Kim <hjkim2902@gmail.com>
2025-02-04 13:53:40 +0900

    feat : Add Account InMemory DBs

---
+++ src/main/java/com/example/adapter/out/persistence/AccountInMemoryDB.java
@@ -0,0 +1,67 @@
+package com.example.adapter.out.persistence;
+
+import com.example.domain.Account;
+
+import java.util.Objects;
+
+abstract class AccountInMemoryDB implements AccountRepository {
+
+    private Long pk = 1L;
+
+    abstract void putAccount(Account account);
+
+    @Override
+    public int getBalance(long accountId, String password) throws IllegalArgumentException {
+        Account account = findAccountById(accountId);
+        validateAccount(account);
+        validatePassword(account, password);
+        return account.getBalance();
+    }
+
+    @Override
+    public boolean deposit(long accountId, int amount) throws IllegalArgumentException  {
+        Account account = findAccountById(accountId);
+        validateAccount(account);
+        return account.deposit(amount);
+    }
+
+    @Override
+    public boolean withdraw(long accountId, String password, int amount) throws IllegalArgumentException {
+        Account account = findAccountById(accountId);
+        validateAccount(account);
+        validatePassword(account, password);
+        return account.withdraw(amount);
+    }
+
+    @Override
+    public long createAccount(String password, int balance) throws IllegalArgumentException{
+        Long accountId = currentPK();
+        increasePK();
+
+        Account account;
+        account = new Account(accountId, password, balance);
+
+        putAccount(account);
+        return account.getAccountId();
+    }
+
+    private void validateAccount(Account account) throws IllegalArgumentException  {
+        if (Objects.isNull(account)) {
+            throw new IllegalArgumentException(account.getAccountId() + "는 존재하지 않는 계좌번호입니다.");
+        }
+    }
+
+    private void validatePassword(Account account, String password) throws IllegalArgumentException  {
+        if (!account.checkPassword(password)) {
+            throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
+        }
+    }
+
+    protected Long currentPK() {
+        return this.pk;
+    }
+
+    protected void increasePK() {
+        this.pk++;
+    }
+}
---
+++ src/main/java/com/example/adapter/out/persistence/AccountListDB.java
@@ -0,0 +1,27 @@
+package com.example.adapter.out.persistence;
+
+import com.example.domain.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountListDB extends AccountInMemoryDB {
+    protected List<Account> db;
+
+    public AccountListDB() {
+        db = new ArrayList<>();
+    }
+
+    @Override
+    public Account findAccountById(long accountId){
+        return this.db.stream()
+                .filter(x -> x.getAccountId()==accountId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    protected void putAccount(Account account) {
+        this.db.add(account);
+    }
+}
