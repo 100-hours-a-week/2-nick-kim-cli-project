cb24878d5fb6643260d304dd0ecedc7568baa6f9
Han Ju Kim <hjkim2902@gmail.com>
2025-02-12 13:10:39 +0900

    fix : 잔액 조회 시 환율 조회

--- src/main/java/com/example/system/handler/controller/BalanceInquiryController.java
+++ src/main/java/com/example/system/handler/controller/BalanceInquiryController.java
@@ -1,19 +1,26 @@
 package com.example.system.handler.controller;
 
 import com.example.account.application.service.AccountQueryUseCase;
-import com.example.system.validate.AccountInputValidator;
+import com.example.exchange.application.service.ExchangeRateQueryUseCase;
+import com.example.exchange.domain.ExchangeRate;
 import com.example.system.io.IOHandler;
+import com.example.system.validate.AccountInputValidator;
+
+import java.time.format.DateTimeFormatter;
 
 public
 class BalanceInquiryController implements MenuController {
     private final AccountQueryUseCase accountQueryUseCase;
+    private final ExchangeRateQueryUseCase exchangeRateQueryUseCase;
     private final AccountInputValidator accountInputValidator;
     private final IOHandler ioHandler;
 
     public BalanceInquiryController(AccountQueryUseCase accountQueryUseCase,
+                                    ExchangeRateQueryUseCase exchangeRateQueryUseCase,
                                     AccountInputValidator accountInputValidator,
                                     IOHandler ioHandler) {
         this.accountQueryUseCase = accountQueryUseCase;
+        this.exchangeRateQueryUseCase = exchangeRateQueryUseCase;
         this.accountInputValidator = accountInputValidator;
         this.ioHandler = ioHandler;
     }
@@ -27,6 +34,13 @@ class BalanceInquiryController implements MenuController {
         String password = accountInputValidator.getValidPassword();
 
         int balance = accountQueryUseCase.getBalance(accountId, password);
-        ioHandler.printMessage(String.format("현재 잔액: %d원", balance));
+        ExchangeRate exchangeRate = exchangeRateQueryUseCase.getUSDExchangeRate();
+        String dateTime = exchangeRate.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        ioHandler.printMessage(String.format("잔액: %d원 / USD 기준: %f$" , balance, exchangeKRWToUSD(balance, exchangeRate.getRate())));
+        ioHandler.printMessage(String.format("%s 기준 환율: 1 USD = %f KRW" , dateTime, exchangeRate.getRate()));
+    }
+
+    private double exchangeKRWToUSD(int balance, double usdExchangeRate) {
+        return 1 / usdExchangeRate * balance;
     }
 }
